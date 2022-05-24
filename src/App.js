9b3659307d49e31f3266751798d6e588363c880f
jsgarry <garryt4@ukr.net>
2022-05-24 20:11:21 +0300

    8. Пагинация. Декомпозиция - Фунд. курс 2:11:42

--- src/App.js
+++ src/App.js
@@ -9,20 +9,29 @@ import { usePosts } from './hooks/usePosts.js';
 import PostService from './API/PostService.js';
 import Loader from './components/UI/loader/Loader.jsx';
 import { useFetching } from './hooks/useFetching.js';
+import { getPageCount } from './utils/pages.js';
+import Pagination from './components/UI/pagination/Pagination.jsx';
 
 function App() {
 
   const [posts, setPosts] = useState([])
   const [filter, setFilter] = useState({sort: '', query: ''})
   const [modal, setModal] = useState(false)
+  const [totalPages, setTotalPages] = useState(0)
+  const [limit, setLimit] = useState(10)
+  const [page, setPage] = useState(1)
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
-    const posts = await PostService.getAll()
-    setPosts(posts)
+
+
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
+    const response = await PostService.getAll(limit, page)
+    setPosts(response.data)
+    const totalCount = response.headers['x-total-count']
+    setTotalPages(getPageCount(totalCount, limit));
   })
 
   useEffect(() => {
-    fetchPosts()
+    fetchPosts(limit, page)
   }, [])
 
   const createPost = (newPost) => {
@@ -36,6 +45,10 @@ function App() {
     setPosts(posts.filter(p => p.id !== post.id))
   }
 
+  const changePage = (page) => {
+    setPage(page)
+    fetchPosts(limit, page)
+  }
 
   return (
     <div className="App">
@@ -58,6 +71,11 @@ function App() {
         ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
         : <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Посты по JS"/>
       }
+      <Pagination
+        page={page}
+        changePage={changePage}
+        totalPages={totalPages}
+      />      
     </div>
   );
 }
@@ -65,4 +83,4 @@ function App() {
 export default App;
 
 // 24.05.22
-// timecode: 1:54:18
+// timecode: 2:11:42
